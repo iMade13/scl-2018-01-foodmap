fb9b1a693e923f7c48b0d85e2f5e642e2c2a1178
Madeleine Marcano <madeleine1302@gmail.com>
2018-07-25 22:26:05 -0400

    funcion de google maps

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,9 @@
-s
+function initMap() {
+    // The location of Uluru
+    var uluru = { lat: -1.000, lng: 100.200 };
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), { zoom: 4, center: uluru });
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({ position: uluru, map: map });
+}
